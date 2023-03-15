a0025060d07523a60388d3757cc0aac877fae4e9
minwachya <minwachya@gmail.com>
2023-03-15 13:08:48 +0900

    [feat/test] test controller에 다양한 path 연결 메서드 추가

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,14 +1,29 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test")
 public class TestController {
+    // 1. /test
     @GetMapping // Get 요청 시 동작
     public String testController() {
-        return "Hellow World!";
+        return "Hello, World!";
+     }
+
+    // 2. test/testGetMapping
+    @GetMapping("/testGetMapping")
+    public String testControllerGetMapping() {
+        return "Hello, World!";
     }
+
+    // 3. test/{id}
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+        return "Hello, World! id: " + id;
+    }
+
 }
