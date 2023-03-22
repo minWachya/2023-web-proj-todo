7ffca58c3f27c1151a0437ff0179a88953120bc0
minwachya <minwachya@gmail.com>
2023-03-22 12:54:44 +0900

    [feat] todo repository 사용하여 todoService에 todo entity 생성, 저장, 검색 기능 추가

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,10 +1,23 @@
 package com.example.demo.service;
 
+import com.example.demo.model.TodoEntity;
+import com.example.demo.persistence.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+    // TodoRepository 인터페이스 구현한 클래스를 JPA가 자동 생성, 자동 주입
+    @Autowired
+    private TodoRepository repository;
+
     public String testService() {
-        return "Test Service";
+        // 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        // 저장
+        repository.save(entity);
+        // 검색
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+        return savedEntity.getTitle();
     }
 }
