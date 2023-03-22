446f33d6dc263d30afd5254a2c68f793a46e3acc
minwachya <minwachya@gmail.com>
2023-03-22 12:14:34 +0900

    [feat/test] todoController에todo service 사용하여 응답 보내는 함수 추가

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TodoDTO;
 import com.example.demo.model.TodoEntity;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,21 @@ import java.util.List;
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+    // 자동 주입
+    @Autowired
+    private TodoService service;
+
+    @GetMapping("test")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testService();
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
 
     /*
+    과제:
      메핑 URL: localhost:8080/todo/testTodo
      반환 값: (TodoDTO 객체와 status: 200)
         반환할 TodoDTO 객체: { “id”:”123”, “title”:”[본인이름]”, “done”: true }
