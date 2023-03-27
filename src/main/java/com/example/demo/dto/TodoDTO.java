2eddd7fbc6d4bfccc57b2201019fc6d49c0e04c3
minwachya <minwachya@gmail.com>
2023-03-27 15:27:11 +0900

    [feat] TodoDTO에 toEntity() 함수 생성

--- src/main/java/com/example/demo/dto/TodoDTO.java
+++ src/main/java/com/example/demo/dto/TodoDTO.java
@@ -20,4 +20,13 @@ public class TodoDTO {
         this.title = entity.getTitle();
         this.done = entity.isDone();
     }
+
+    // Controller가 받은 TodoDTO를 TodoEntity로 변환 필요
+    public static TodoEntity toEntity(final TodoDTO dto) {
+        return TodoEntity.builder()
+                .id(dto.getId())
+                .title(dto.getTitle())
+                .done(dto.isDone())
+                .build();
+    }
 }
