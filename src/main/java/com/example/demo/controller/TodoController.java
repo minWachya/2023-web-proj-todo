1e677551be2c3b6ce4258faded1491bf86b94646
minwachya <minwachya@gmail.com>
2023-03-27 15:51:20 +0900

    [feat] Contoroller에 todo entity 추가하는 함수(createTodo) 생성

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -6,12 +6,11 @@ import com.example.demo.model.TodoEntity;
 import com.example.demo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -39,5 +38,32 @@ public class TodoController {
     public ResponseEntity<TodoDTO> todoControllerResponseEntity() {
         TodoDTO response = TodoDTO.builder().id("123").title("이민영").done(true).build();
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user";
+
+            // dto로 entity 생성
+            TodoEntity entity = TodoDTO.toEntity(dto);
+            // id null처리
+            entity.setId(null);
+            // userid에 임시 id 저장
+            entity.setUserId(temporaryUserId);
+            // entity 저장 후 userid가 생성한 todo entity 목록 받아옴
+            List<TodoEntity> entities = service.create(entity);
+            // todo entity 목록을 todo dto 목록으로 변경
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+            // response 만들어서 출력
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder()
+                    .data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 }
