b6028a3334adeb2bf3ea1ba43ea4e7863580bf9b
minwachya <minwachya@gmail.com>
2023-03-29 12:17:04 +0900

    [feat] service에 toso 수정하는 함수(update) 생성

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j  // 로깅 레벨 관리해주는 라이브러리(Simple Logging Facade for Java)
 @Service
@@ -34,10 +35,29 @@ public class TodoService {
         return repository.findByUserId(entity.getUserId());
     }
 
+    // 수정 후 모든 Todo List 반환
+    public List<TodoEntity> update(final TodoEntity entity) {
+        validate(entity);
+
+        // entity가 Null이 아닌 경우 실행
+        final Optional<TodoEntity> optional = repository.findById(entity.getId());
+        optional.ifPresent(todo -> {
+            // title, done 수정 후 저장
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            repository.save(todo);
+        });
+
+        return retrieve(entity.getUserId());
+    }
+
+    // userId가 생성한 모든 Todo List 리턴
     public List<TodoEntity> retrieve(final String userId) {
         return repository.findByUserId(userId);
     }
 
+    // 엔티티 유효성 확인
     private static void validate(TodoEntity entity) {
         if(entity == null) {
             log.warn("Entity cannot be null");
