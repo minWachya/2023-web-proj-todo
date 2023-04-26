3b9fe511c1c8a2e073f5bc78d406213f6450faf6
minwachya <minwachya@gmail.com>
2023-04-26 12:42:01 +0900

    [feat] 사용자 인증 구현 시작. UserEntity 생성

---
+++ src/main/java/com/example/demo/model/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")}) // DDL 생성 시 유니크 제약 조건
+public class UserEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;   // id와 같이 고유한 기능을 함
+
+    @Column(nullable = false)
+    private String password;
+}
