82c301dbd245755bec782dd25b75717c1bdd160a
minwachya <minwachya@gmail.com>
2023-04-26 13:00:26 +0900

    [feat] 사용자 인증을 위한 User Service 생성(사용자 저장, 사용자 정보 반환)

---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.demo.service;
+
+import com.example.demo.model.UserEntity;
+import com.example.demo.persistence.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    // User 생성
+    public UserEntity create(final UserEntity userEntity) {
+        // 엔티티가 null이거나 email이 없으면 오류
+        if(userEntity == null || userEntity.getEmail() == null) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        // 같은 email이 존재하는지 확인
+        final String email = userEntity.getEmail();
+        if(userRepository.existsByEmail(email)) {
+            log.warn("Email already exists {}", email);
+            throw new RuntimeException("Email already exists");
+        }
+        // 새 이메일이면 엔티티 저장하기
+        return userRepository.save(userEntity);
+    }
+
+    // email과 pw를 가지고 사용자 정보 반환하기
+    public UserEntity getByCredentials(final String email, final String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+}
