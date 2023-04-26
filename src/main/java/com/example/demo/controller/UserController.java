2ea977076ae54828dd9155631e89d171a08ab718
minwachya <minwachya@gmail.com>
2023-04-26 13:20:55 +0900

    [feat] User Controller 생성

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,75 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            // 요청받은 사용자 정보 DTO를 토대로 UserEntity 생성
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .id(userDTO.getId())
+                    .username(userDTO.getUsername())
+                    .build();
+            // 생성한 UserEntity를 서버에 저장후 저장한 UserEntity 받아옴
+            UserEntity registerUser = userService.create(user);
+            // 받아온 User 정보를 DTO로 변환
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(registerUser.getEmail())
+                    .id(registerUser.getId())
+                    .username(registerUser.getUsername())
+                    .build();
+            // 변환한 DTO 반환
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            // 사용자 정보는 항상 하나이므로 리스트로 만들지 않고 그냥 UserDTO 리턴
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    // 로그인
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO) {
+        // 요청받은 사용자 정보가 서버에 있는지 확인 후 반환
+        UserEntity user = userService.getByCredentials(
+                userDTO.getEmail(),
+                userDTO.getPassword()
+        );
+
+        // 사용자 정보가 있으면
+        if(user != null) {
+            // userDTO 만들어서 반환
+            final UserDTO responseUserDTO = UserDTO.builder()
+                    .email(user.getEmail())
+                    .id(user.getId())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        } else {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login failed")
+                    .build();
+
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+}
